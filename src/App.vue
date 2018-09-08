6e6d0da09d4c518357fbeb33d615ead7ef6ede45
kaikai1818 <704316333@qq.com>
2018-09-09 07:06:03 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~assets/stylus/variable";
+#app {
+  
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
