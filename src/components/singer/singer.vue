09f259177b35f3b3244d56dc206daf41832d076a
kaikai1818 <704316333@qq.com>
2018-10-07 14:40:29 +0800

    third01

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
+  <div class="singer">
     singer
   </div>
 </template>
+<script>
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(this.singers)
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .singer
+    position fixed
+    top 88px
+    bottom 0
+    width 100%
+</style>
